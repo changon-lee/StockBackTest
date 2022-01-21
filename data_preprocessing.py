d0f380189cdcdc6cc063f69bc77d8935b85060a9
changon-lee <cvos2213@gmail.com>
2022-01-21 17:38:26 +0900

    fix code

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,9 +1,12 @@
 from parameter import *
 import urllib.request
+import os
 
 def load_stock_historical_data():
+    stock_directory_name = 'input_stock'
     for stock_name in stock_list:
-        print(historical_data_url_prefix + stock_name + historical_data_url_suffix)
         url = historical_data_url_prefix + stock_name + historical_data_url_suffix
-        path = 'input_stock/' + stock_name + '.csv'
+        if not os.path.exists(stock_directory_name):
+            os.mkdir(stock_directory_name)
+        path = stock_directory_name + stock_name + '.csv'
         urllib.request.urlretrieve(url, path)
