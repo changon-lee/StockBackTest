def2d43aa8258db90733013ca3e8cdb88e125de0
changon-lee <cvos2213@gmail.com>
2022-01-21 17:39:15 +0900

    fix code

---
+++ parameter.py
@@ -0,0 +1,5 @@
+stock_directory = 'input_stock'
+stock_list = ['TQQQ', 'QQQ', 'SQQQ']
+
+historical_data_url_prefix = 'https://query1.finance.yahoo.com/v7/finance/download/'
+historical_data_url_suffix = '?period1=0&period2=2000000000&interval=1d&events=history&includeAdjustedClose=true'
